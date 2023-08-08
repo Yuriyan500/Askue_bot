a0eeab4d211f19f4ac851f8176d2e8507c618a7d
yuriyan500 <yuriyan500@gmail.com>
2023-08-08 10:14:56 +0700

    Добавлен файл .env для хранения токена бота BOT_TOKEN. Добавлен файл config_reader.py, предназначенный для работы с файлом .env (для чтения токена бота в виде SecretStr вместо str). В main.py изменена строка создания объекта бота через config.bot_token.get_secret_value(), где config - объект класса Settings в config_reader.py. В requirements.txt добавлен pydantic и pydantic_settings.

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.enums.dice_emoji import DiceEmoji
-
-import config
+from config_reader import config
 
 # Включаем логгирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
+
 # Объект бота
-bot = Bot(token=config.BOT_API_TOKEN)
+# Для записей с типом Secret* необходимо
+# вызывать метод get_secret_value(),
+# чтобы получить настоящее содержимое вместо '*******'
+# bot = Bot(token=config.BOT_API_TOKEN)
+bot = Bot(token=config.bot_token.get_secret_value())
 # Диспетчер
 dp = Dispatcher()
 
